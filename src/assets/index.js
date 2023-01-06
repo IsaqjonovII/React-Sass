dc0387f9bee041895250feadd67e772811e4ab83
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-01-06 12:22:55 +0500

    small changes

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+// import ChangeIcon from "./change.svg";
+// import GiftIcon from "./gift.svg";
+// import DiscountIcon from "./discount.svg";
+
+// export { GiftIcon, ChangeIcon, DiscountIcon };
---
+++ src/mocks/data.js
@@ -0,0 +1,52 @@
+// import { ChangeIcon, DiscountIcon, GiftIcon, img } from "../assets";
+
+export const PRODUCTS__DATA = [
+  {
+    id: 0,
+    title: "Samsung Galaxy Z flip",
+    price: 999,
+    image: "https://raw.githubusercontent.com/ilhomjon003/next-js-task/acc67353092b006c64116841db8861bd6c3f5062/assets/change.svg",
+    dividedpayment: 333,
+    label: ["https://raw.githubusercontent.com/ilhomjon003/next-js-task/acc67353092b006c64116841db8861bd6c3f5062/assets/change.svg", "https://raw.githubusercontent.com/ilhomjon003/next-js-task/acc67353092b006c64116841db8861bd6c3f5062/assets/change.svg"],
+  },
+//   {
+//     id: 1,
+//     title: "iPhone 14 pro max",
+//     price: 1099,
+//     image: img,
+//     dividedpayment: 333,
+//     label: [GiftIcon, ChangeIcon],
+//   },
+//   {
+//     id: 2,
+//     title: "Samsung Galaxy Z flip",
+//     price: 999,
+//     image: img,
+//     dividedpayment: 333,
+//     label: [ChangeIcon],
+//   },
+//   {
+//     id: 3,
+//     title: "iPhone 14 pro max",
+//     price: 1299,
+//     image: img,
+//     dividedpayment: 333,
+//     label: [GiftIcon, ChangeIcon],
+//   },
+//   {
+//     id: 4,
+//     title: "Samsung Galaxy Z flip",
+//     price: 600,
+//     image: img,
+//     dividedpayment: 333,
+//     label: [ChangeIcon],
+//   },
+//   {
+//     id: 5,
+//     title: "iPhone 13 pro max",
+//     price: 1090,
+//     image: img,
+//     dividedpayment: 333,
+//     label: [GiftIcon, ChangeIcon],
+//   },
+];
